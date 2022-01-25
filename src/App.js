1b2d28713f6761e836a9a6cb234f542bc1bd64ee
Kishan <ks805556@dal.ca>
2022-01-25 17:35:53 -0400

    custom changes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>This is my deployment for CSCI5709 !!!</h2>
+        <h3>Tutorial 02</h3>
       </header>
     </div>
   );
