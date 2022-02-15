b0c83602740de7420c12052bee9b22e2f3ba0838
Kishan <ks805556@dal.ca>
2022-02-15 13:29:02 -0400

    tutorial04 created

---
+++ Kishan_Kahodariya_README.md.md
@@ -0,0 +1,26 @@
+# Tutorial 04
+
+- _Date Created_: 10 Feb 2022
+- _Last Modification Date_: 15 Feb 2022
+- _Git URL_: https://git.cs.dal.ca/kahodariya/kishan_kahodariya-csci5709/-/tree/tutorial03
+- _Git Group Repo URL_: https://git.cs.dal.ca/kahodariya/csci-5709-group-06-project
+- _Git Group Repo Individual branch URL_: https://git.cs.dal.ca/kahodariya/csci-5709-group-06-project/-/tree/kishan
+
+## Authors
+
+- [Kishan Kahodariya](ks805556@dal.ca) - _(Student)_
+
+## Getting Started
+
+- npm install react --save
+- npm install react-bootstrap
+- npm install bootstrap
+- npm install react-router-dom
+
+### Installing
+
+- Download and install nodejs from https://nodejs.org/en/
+
+## Deployment
+
+- _Deployment URL_: https://kishan-kahodariya-tutorial-03.herokuapp.com/
---
+++ src/Loginpage.jsx
@@ -0,0 +1,70 @@
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import {useNavigate} from "react-router-dom";
+import { useState } from 'react';
+import './Loginpage.css';
+
+
+const Loginpage = () => {
+
+  let navigate= useNavigate();
+  const [credentials, setCredentials]=useState({
+    email:"",
+    password:""
+  })
+
+   const updateCredentials=(e)=>{
+      setCredentials({
+        ...credentials,
+        [e.target.name]:e.target.value
+      })
+      console.log(credentials); 
+   }
+
+  
+   const postData=async(e)=>{
+     e.preventDefault()
+    let postURL='https://tutorial4-api.herokuapp.com/api/users/login';
+    let dataToSubmit=credentials;
+    try
+    {
+
+      const data=await fetch(postURL,{
+       method:'POST',
+       headers:{
+         "Content-Type":"application/json",
+         "Accept":"application/json"
+       },
+       body:JSON.stringify(dataToSubmit)
+     })
+     console.log(data)
+     if(data.status){
+          navigate("/userlist")
+      }else{
+        alert("Invalid Email/ Password");
+      }
+    }catch(e){
+        console.log(e)
+    }
+
+   }
+  return (
+    <Container id="main-container" className="d-grid h-100">
+      <Form id="sign-in-form" className="text-center p-3 w-100" onSubmit={postData}>
+        <h1 className="mb-3 fs-3 fw-normal">SIGN IN</h1>
+        <Form.Group controlId="sign-in-email-address">
+          <Form.Control value={credentials.email} onChange={updateCredentials} name='email' type="email" size="lg" placeholder="Email address" autoComplete="username" className="position-relative" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="sign-in-password">
+          <Form.Control value={credentials.password} onChange={updateCredentials} name='password' type="password" size="lg" placeholder="Password" autoComplete="current-password" className="position-relative" />
+        </Form.Group>
+        <div className="d-grid">
+          <Button type="submit" variant="primary" size="lg" >Sign in</Button>
+        </div>
+      </Form>
+    </Container>
+  );
+}
+
+export default Loginpage;
